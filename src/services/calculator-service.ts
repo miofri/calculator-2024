b14356501e42d4f9318ddd44ce104eed97d7cd80
selvi <slvkesuma@gmail.com>
2024-01-30 17:19:41 +0200

    fixed no check for friday total

--- src/services/calculator-service.ts
+++ src/services/calculator-service.ts
@@ -3,14 +3,14 @@ import { deliveryFeeCalculation } from './utils/deliveryFeeCalculation';
 
 const BASE_FEE = 2; //euros
 const MAX_FEE = 15; //euros
-const SMALL_PURCHASE_THRESHOLD = 10; //euros
+const SMALL_PURCHASE_FEE_THRESHOLD = 10; //euros
 
-const BULK_THRESHOLD = 5; //items
-const BULK_SURCHARGE = 0.5; //euros
-const BIG_BULK_THRESHOLD = 12; //items
-const BIG_BULK_SURCHARGE = 1.2; //euros
+const BULK_ITEM_THRESHOLD = 5; //items
+const BULK_SURCHARGE_FEE = 0.5; //euros
+const BIG_BULK_ITEM_THRESHOLD = 12; //items
+const BIG_BULK_SURCHARGE_FEE = 1.2; //euros
 
-const FREE_DELIVERY_THRESHOLD = 200; //meters
+const FREE_DELIVERY_DISTANCE_THRESHOLD = 200; //meters
 const BASE_DELIVERY_DISTANCE = 1000; //meters
 const DELIVERY_DISTANCE_THRESHOLD = 500; //meters
 
@@ -24,17 +24,17 @@ export const deliveryCalculationResult = (formData: FormStates): number => {
 	);
 
 	const cartValueSurcharge =
-		formData.cartValue < SMALL_PURCHASE_THRESHOLD
-			? SMALL_PURCHASE_THRESHOLD - formData.cartValue
+		formData.cartValue < SMALL_PURCHASE_FEE_THRESHOLD
+			? SMALL_PURCHASE_FEE_THRESHOLD - formData.cartValue
 			: 0;
 
 	let bulkSurcharge =
-		formData.amountOfItems < BULK_THRESHOLD
+		formData.amountOfItems < BULK_ITEM_THRESHOLD
 			? 0
-			: (formData.amountOfItems - 4) * BULK_SURCHARGE;
+			: (formData.amountOfItems - 4) * BULK_SURCHARGE_FEE;
 
-	if (formData.amountOfItems > BIG_BULK_THRESHOLD) {
-		bulkSurcharge = bulkSurcharge + BIG_BULK_SURCHARGE;
+	if (formData.amountOfItems > BIG_BULK_ITEM_THRESHOLD) {
+		bulkSurcharge = bulkSurcharge + BIG_BULK_SURCHARGE_FEE;
 	}
 
 	const total = deliveryFee + cartValueSurcharge + bulkSurcharge;
@@ -43,12 +43,14 @@ export const deliveryCalculationResult = (formData: FormStates): number => {
 	const isFriday = dateOfOrder.getDay() === 5;
 	const hour = dateOfOrder.getHours();
 
-	if (formData.cartValue! >= FREE_DELIVERY_THRESHOLD) return 0;
+	if (formData.cartValue! >= FREE_DELIVERY_DISTANCE_THRESHOLD) return 0;
+
+	if (isFriday && hour >= 15 && hour < 19) {
+		const finalTotal = Math.round(total * 1.2 * 10) / 10;
+		return finalTotal > MAX_FEE ? MAX_FEE : finalTotal;
+	}
 
 	if (total > 15) return 15;
-
-	if (isFriday && hour >= 15 && hour < 19)
-		return Math.round(total * 1.2 * 10) / 10;
 
 	return total;
 };
