2145a4801ec25eb623c359fc8db2adce20475a01
selvi <slvkesuma@gmail.com>
2024-01-30 17:50:20 +0200

    testing height issue

--- src/ui-components/CalculatorUI.tsx
+++ src/ui-components/CalculatorUI.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const BodyBackground = styled.div`
-	width: 100vw;
-	height: 100vh;
+	width: 100%;
+	height: 100%;
+	min-height: 100vh;
 	background-color: ${(props) => props.theme.bodyBackground};
 	display: flex;
 	flex-direction: column;
